ab199ff76d1e7cdca036b567c86407c64aa10b78
Liam <liargent@hotmail.com>
2023-06-11 19:48:20 +0100

    Add code to test out binary tree

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import './style.css';
 import Tree from './tree';
 
-// let myAmazingTree = new Tree([2, 3, 6, 7, 9, 28, 67, 97]);
-// works with 3 numbers but no more or less
-let myAmazingTree = new Tree(
-    [200, 2, 4, 6, 8, 14, 28, 82, 300].sort((a, b) => a > b)
-);
+let myAmazingTree = new Tree([200, 2, 4, 6, 8, 14, 28, 82, 300]);
 Tree.prettyPrint(myAmazingTree.root);
